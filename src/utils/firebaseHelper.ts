2dc779ddad6e2722435e90857ba91fcd9b2b553b
pafinkbeiner <paul-d-f@web.de>
2022-07-04 19:09:19 +0200

    changes to firebase and ci / cd test

--- src/utils/firebaseHelper.ts
+++ src/utils/firebaseHelper.ts
@@ -1,11 +1,7 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from "firebase/app";
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  sendEmailVerification,
-  signInWithEmailAndPassword
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,11 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export {
-  auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendEmailVerification
-}
+// Initialize Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
