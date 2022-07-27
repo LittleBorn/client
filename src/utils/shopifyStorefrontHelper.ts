7a5f7b54bb30b484b94960bf736927c2169a6c20
pafinkbeiner <paul-d-f@web.de>
2022-07-27 21:56:05 +0200

    changed environment

--- src/utils/shopifyStorefrontHelper.ts
+++ src/utils/shopifyStorefrontHelper.ts
@@ -6,7 +6,7 @@ export const sendStorefrontQuery = <T>(data: string) => {
     method: 'post',
     url: 'https://littleborn.myshopify.com/api/2022-07/graphql.json',
     headers: { 
-      'X-Shopify-Storefront-Access-Token': '28869d2b1c2ac7dbd3998dfed945cd2a', 
+      'X-Shopify-Storefront-Access-Token': process.env.REACT_APP_SHOPIFY_TOKEN || "28869d2b1c2ac7dbd3998dfed945cd2a", 
       'Content-Type': 'application/json'
     },
     data : data
