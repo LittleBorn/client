b323d958a87696671056fca99d749ba4457cbebe
pafinkbeiner <paul-d-f@web.de>
2022-07-31 17:32:47 +0200

    persited login state in localStorage - check index.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonText } from '@ionic/react';
+import { useEffect } from 'react';
 import MainTemplate from '../components/MainTemplate';
 import { IPagePros } from '../interfaces/IPageProps';
-import { accessToken$ } from '../stores/userStore';
 import './Home.css';
 
 const Home: React.FC<IPagePros> = ({props}: IPagePros) => {
 
-  const logout = () => {
-    accessToken$.next(undefined);
-  }
+  useEffect(() => {
+    
+    // get user state
+  
+    return () => {
+      
+    }
+  }, [])
+  
 
   return (
     <MainTemplate>
-        <h1>Home Component</h1>
-        <button onClick={logout}>Logout</button>
+      <div style={{display: "flex", flexDirection: "column", height: "100vh", padding: "1rem", gap: "1rem"}}>
+        <IonText style={{fontSize: "1.5em"}}><b>Hallo</b></IonText>
+      </div>
     </MainTemplate>
   );
 };
