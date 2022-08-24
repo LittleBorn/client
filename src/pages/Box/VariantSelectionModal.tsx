ce1c85d18647003ad1eff8960e3392a27208d140
pafinkbeiner <paul-d-f@web.de>
2022-08-24 12:24:06 +0200

    sc

--- src/pages/Box/VariantSelectionModal.tsx
+++ src/pages/Box/VariantSelectionModal.tsx
@@ -1,5 +1,6 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonModal, IonText, IonTitle, IonToolbar } from "@ionic/react";
 import { useState } from "react";
+import Button from "../../components/Button";
 import { IShopifyProduct } from "../../interfaces/Shopify/IShopifyProduct";
 import { IShopifyProductVariant } from "../../interfaces/Shopify/IShopifyProductVariant";
 import { addItemToBasket } from "../../stores/basketStore";
@@ -12,6 +13,9 @@ interface ContainerProps {
 
 const VariantSelectionModal: React.FC<ContainerProps> = ({ isOpen, setIsOpen, product }) => {
 
+    const [selectedVariant, setSelectedVariant] = useState(undefined);
+    const [selectedAmount, setSelectedAmount] = useState(1)
+
     return (
         <IonModal isOpen={isOpen}>
             <IonHeader>
@@ -19,20 +23,29 @@ const VariantSelectionModal: React.FC<ContainerProps> = ({ isOpen, setIsOpen, pr
                     <IonButtons slot="start">
                         <IonButton onClick={() => setIsOpen(false)}>Cancel</IonButton>
                     </IonButtons>
-                    <IonTitle>Welcome</IonTitle>
+                    <IonTitle>Varianten</IonTitle>
                     <IonButtons slot="end">
-                        <IonButton strong={true} onClick={() => {addItemToBasket(product.node.id); setIsOpen(false)}}>
+                        <IonButton strong={true} onClick={() => { addItemToBasket(product.node.id); setIsOpen(false) }}>
                             Confirm
                         </IonButton>
                     </IonButtons>
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                {
-                    product.node.variants.edges.map((variant: IShopifyProductVariant) => {
-                        return <IonText key={variant.node.id}>{variant.node.title}</IonText>
-                    })
-                }
+                <div>
+                    {
+                        product.node.variants.edges.map((variant: IShopifyProductVariant) => {
+                            return <IonText key={variant.node.id}>{variant.node.title}</IonText>
+                        })
+                    }
+                </div>
+
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
+                    <Button title="-" onClick={() => selectedAmount > 1 ? setSelectedAmount(selectedAmount - 1) : setSelectedAmount(selectedAmount)} />
+                    <IonText>{selectedAmount}</IonText>
+                    <Button title="+" onClick={() => setSelectedAmount(selectedAmount + 1)} />
+                </div>
+
             </IonContent>
         </IonModal>
     );
